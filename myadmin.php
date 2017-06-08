f05305e4c33132d5116dd8f91aae564b2bf933e5
Joe Huss <detain@interserver.net>
2017-06-08 00:59:10 -0400

    plugin definition updates

--- myadmin.php
+++ myadmin.php
@@ -8,17 +8,17 @@ return [
 	'name' => 'Cpanel Webhosting',
 	'description' => 'Allows selling of Cpanel Server and VPS License Types.  More info at https://www.netenberg.com/cpanel.php',
 	'help' => 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a cpanel license. Allow 10 minutes for activation.',
-	'module' => 'licenses',
+	'module' => 'webhosting',
 	'author' => 'detain@interserver.net',
 	'home' => 'https://github.com/detain/myadmin-cpanel-webhosting',
 	'repo' => 'https://github.com/detain/myadmin-cpanel-webhosting',
 	'version' => '1.0.0',
-	'type' => 'licenses',
+	'type' => 'service',
 	'hooks' => [
 		/*'function.requirements' => ['Detain\MyAdminCpanel\Plugin', 'Requirements'],
-		'licenses.settings' => ['Detain\MyAdminCpanel\Plugin', 'Settings'],
-		'licenses.activate' => ['Detain\MyAdminCpanel\Plugin', 'Activate'],
-		'licenses.change_ip' => ['Detain\MyAdminCpanel\Plugin', 'ChangeIp'],
+		'webhosting.settings' => ['Detain\MyAdminCpanel\Plugin', 'Settings'],
+		'webhosting.activate' => ['Detain\MyAdminCpanel\Plugin', 'Activate'],
+		'webhosting.change_ip' => ['Detain\MyAdminCpanel\Plugin', 'ChangeIp'],
 		'ui.menu' => ['Detain\MyAdminCpanel\Plugin', 'Menu'] */
 	],
 ];
