c484d005e4f7e45c7da6cee7ff71f8fa3f9c0723
Joe Huss <detain@interserver.net>
2017-06-24 20:14:43 -0400

    capitolization udpates

--- src/Plugin.php
+++ src/Plugin.php
@@ -42,7 +42,7 @@ class Plugin {
 			$serverdata = get_service_master($serviceInfo[$settings['PREFIX'].'_server'], self::$module);
 			$hash = $serverdata[$settings['PREFIX'].'_key'];
 			$ip = $serverdata[$settings['PREFIX'].'_ip'];
-			$success = true;
+			$success = TRUE;
 			$extra = run_event('parse_service_extra', $serviceInfo[$settings['PREFIX'] . '_extra'], self::$module);
 			function_requirements('whm_api');
 			$user = 'root';
