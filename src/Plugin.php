1d540a60fb6d1f0ce49b5b20cf0154f8bc40e541
Joe Huss <detain@interserver.net>
2020-02-13 19:04:20 -0500

    Added detection of  'is a reserved username on this system' in the response message for CPanel website activation and to try new usernames till it gets one that works when it finds it  (along with the existing 'This system already has an account named', 'The name of another account on this server has the same initial', and 'Sorry, a group for that username already exists'  pattern matching)

--- src/Plugin.php
+++ src/Plugin.php
@@ -165,8 +165,8 @@ class Plugin
 					$response = json_decode($response, true);
 				}
 			}
-			if (preg_match("/^.*This system already has an account named .{1,3}{$username}.{1,3}\.$/m", $response['result'][0]['statusmsg']) || preg_match('/^.*The name of another account on this server has the same initial/m', $response['result'][0]['statusmsg'])) {
-				while (preg_match("/^.*This system already has an account named .{1,3}{$username}.{1,3}\.$/m", $response['result'][0]['statusmsg']) || preg_match('/^.*The name of another account on this server has the same initial/m', $response['result'][0]['statusmsg'])) {
+			if (preg_match("/^.*This system already has an account named .{1,3}{$username}.{1,3}\.$/m", $response['result'][0]['statusmsg']) || preg_match('/^.*The name of another account on this server has the same initial/m', $response['result'][0]['statusmsg']) || preg_match('/^.*is a reserved username on this system/m', $response['result'][0]['statusmsg'])) {
+				while (preg_match("/^.*This system already has an account named .{1,3}{$username}.{1,3}\.$/m", $response['result'][0]['statusmsg']) || preg_match('/^.*The name of another account on this server has the same initial/m', $response['result'][0]['statusmsg']) || preg_match('/^.*is a reserved username on this system/m', $response['result'][0]['statusmsg'])) {
 					$username .= 'a';
 					$username = mb_substr($username, 1);
 					$options['username'] = $username;
